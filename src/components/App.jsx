3ee777b244b362e36999956d12fd24d09dfb9926
abdullah salameh <abdullahsalameh66@gmail.com>
2024-02-24 14:29:35 +0300

    adding a form element for search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Searchinput from "./SearchInput";
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <div className="ui massive icon input">
+              <input type="text" placeholder="Search..." />
+              <i className="search icon"></i>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default App;
