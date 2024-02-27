528f073cd06d59885825abb9c5bbc4840358bb6f
abdullah salameh <abdullahsalameh66@gmail.com>
2024-02-27 13:22:08 +0300

    adding onFormSubmit method

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -2,25 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class SearchInput extends React.Component {
-  onFormSubmit(event){
+  constructor(props) {
+    super(props);
+    this.state = { entry: "" };
+    //this.onFormSubmit=this.onFormSubmit.bind(this);
+  }
+  // we made this function anynoms to not bind the function
+  onFormSubmit = (event) => {
     event.preventDefault();
     // Your form submission logic here
+    console.log(this.state.entry);
   };
-  render(){
+  render() {
     return (
-        <div className="ui segment">
-          <form className="ui form" onSubmit={this.onFormSubmit}>
-            <div className="field">
-              <div className="ui massive icon input">
-                <input type="text" placeholder="Search..." />
-                <i className="search icon"></i>
-              </div>
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <div className="ui massive icon input">
+              <input
+                type="text"
+                placeholder="Search..."
+                onChange={(event) => {
+                  this.setState({ entry: event.target.value });
+                }}
+              />
+              <i className="search icon"></i>
             </div>
-          </form>
-        </div>
-      );
+          </div>
+        </form>
+      </div>
+    );
   }
-
 }
 
 export default SearchInput;
